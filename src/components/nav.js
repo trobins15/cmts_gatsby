f242cbd62c7e6e996c88c607e90fea33500c7fc8
trobins15 <33587650+trobins15@users.noreply.github.com>
2021-12-28 15:49:28 -0600

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -27,6 +27,7 @@ const Nav = ({ navigation, secondaryLinks }) => (
                 <ul
                   id={`extended-nav-section-${idx}`}
                   className="usa-accordion__content usa-nav__submenu"
+                  hidden
                 >
                   {navGroup.items.map((navItem, idx) => (
                     <li key={idx} className="usa-nav__submenu-item">
